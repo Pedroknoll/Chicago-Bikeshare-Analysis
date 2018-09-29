4e2346f0d476c23883b68f319f9e337366268db9
Pedro Knoll <p.giacometo@gmail.com>
2018-09-28 22:30:32 -0300

    feat: Create a script to return the first 20 samples

--- chicago_bikeshare.py
+++ chicago_bikeshare.py
@@ -33,6 +33,11 @@ print("\n\nTASK 1: Printing the first 20 samples")
 # Let's change the data_list to remove the header from it.
 data_list = data_list[1:]
 
+# print the first 20 rows
+# setup the sample
+for sample in data_list[:20]:
+    print(sample)
+
 # We can access the features through index
 # E.g. sample[6] to print gender or sample[-2]
 
